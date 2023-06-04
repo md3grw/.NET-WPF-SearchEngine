0194d621dc0c00e890473f5cb0a60d7a6db43a62
wonderxxfull <emoskalenko97@gmail.com>
2023-06-04 19:24:05 +0300

    basics of the project

---
+++ Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using DotNetEnv;
+
+namespace SearchEngine
+{
+    class Config
+    {
+        public readonly string apiKey;
+        public readonly string searchEngineID;
+
+        public Config()
+        {
+            Env.Load("../../../.env");
+
+            apiKey = Environment.GetEnvironmentVariable("API_KEY");
+            searchEngineID = Environment.GetEnvironmentVariable("SEARCH_ENGINE_ID");
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SearchEngine.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SearchEngine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) MessageBox.Show("Search");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
